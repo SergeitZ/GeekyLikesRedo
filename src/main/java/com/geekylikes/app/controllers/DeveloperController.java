954d38c8cdbca9203a74b8306387d6c64d5e6f97
Sergio Franco <sergiofranco6@gmail.com>
2022-01-21 20:45:44 -0500

    added delete developer and search by cohort

--- src/main/java/com/geekylikes/app/controllers/DeveloperController.java
+++ src/main/java/com/geekylikes/app/controllers/DeveloperController.java
@@ -2,8 +2,11 @@ package com.geekylikes.app.controllers;
 
 import com.geekylikes.app.models.Developer;
 import com.geekylikes.app.repositories.DeveloperRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -20,18 +23,24 @@ public class DeveloperController {
         return repository.findAll();
     }
 
-    @PostMapping
-    public @ResponseBody Developer createDeveloper (@RequestBody Developer newDeveloper) {
-        return repository.save(newDeveloper);
-    }
-
     @GetMapping("/{id}")
     public @ResponseBody Developer getOneDeveloper (@PathVariable Long id) {
         return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
-    @PutMapping("/id")
-    public @ResponseBody Developer updateEmployee (@PathVariable Long id, @RequestBody Developer updates) {
+    @GetMapping("/cohort/{cohort}")
+    public ResponseEntity<List<Developer>> getOneDevelopersByCohort (@PathVariable Integer cohort) {
+        return new ResponseEntity<>(repository.findAllByCohort(cohort, Sort.by("name")), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Developer> createDeveloper (@RequestBody Developer newDeveloper) {
+        return new ResponseEntity<>(repository.save(newDeveloper), HttpStatus.CREATED);
+    }
+
+
+    @PutMapping("/{id}")
+    public @ResponseBody Developer updateDeveloper (@PathVariable Long id, @RequestBody Developer updates) {
         Developer developer = repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
         if (updates.getName() != null) developer.setName(updates.getName());
@@ -40,5 +49,11 @@ public class DeveloperController {
         if (updates.getLanguages() != null) developer.setLanguages(updates.getLanguages());
 
         return repository.save(updates);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> destroyDeveloper (@PathVariable Long id) {
+        repository.deleteById(id);
+        return new ResponseEntity<>("Deleted", HttpStatus.OK);
     }
 }
