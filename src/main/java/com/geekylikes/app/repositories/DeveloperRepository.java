b086123b3e2491cdd42e70e5ca6e2d64afa416fd
Sergio Franco <sergiofranco6@gmail.com>
2022-02-06 19:43:47 -0500

    finished approve system

--- src/main/java/com/geekylikes/app/repositories/DeveloperRepository.java
+++ src/main/java/com/geekylikes/app/repositories/DeveloperRepository.java
@@ -10,6 +10,10 @@ import java.util.List;
 public interface DeveloperRepository extends JpaRepository<Developer, Long> {
 
     List<Developer> findAllByCohort(Integer cohort, Sort sort);
+    List<Developer> findAllByLanguages_id(Long id);
+
+    //get a list of developers that liked geekout
+    List<Developer> findAllByApprovals_geekout_id(Long id);
 
 //    @Query("SELECT * FROM developer WHERE cohort = ?1 AND ?2 in languages")
 //    Developer findByCohortAndLanguage(Integer cohort, String language);
